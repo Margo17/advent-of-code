7e7553040b1425396c77a9f262b16a2d9d943ba2
Martynas Goberis <martynas.goberis@seb.se>
2024-12-02 12:37:49 +0200

    Add Day1 solutions

---
+++ 2025/Day1-HystorianHysteria/Part1/Program.cs
@@ -0,0 +1,27 @@
+﻿var leftList = new List<int>();
+var rightList = new List<int>();
+var listLength = 0;
+var totalDistance = 0;
+
+while (Console.ReadLine() is string line)
+{
+    string[] listNumbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    leftList.Add(int.Parse(listNumbers[0]));
+    rightList.Add(int.Parse(listNumbers[1]));
+
+    listLength++;
+}
+
+for (var index = 0; index < listLength; index++)
+{
+    var leftLowest = leftList.Min();
+    var rightLowest = rightList.Min();
+
+    totalDistance += Math.Abs(rightLowest - leftLowest);
+	
+    leftList.Remove(leftLowest);
+    rightList.Remove(rightLowest);
+}
+
+Console.WriteLine(totalDistance);
---
+++ 2025/Day1-HystorianHysteria/Part2/Program.cs
@@ -0,0 +1,25 @@
+﻿var leftList = new List<int>();
+var rightList = new List<int>();
+var similarityScore = 0;
+
+while (Console.ReadLine() is string line)
+{
+    string[] listNumbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    leftList.Add(int.Parse(listNumbers[0]));
+    rightList.Add(int.Parse(listNumbers[1]));
+}
+
+var leftFirst = leftList.First();
+
+foreach (var number in leftList)
+{
+    var numberAppearancesInRightList = rightList
+        .Where(n => n.Equals(number))
+        .Count();
+    similarityScore += number * numberAppearancesInRightList;
+}
+
+leftList.RemoveAt(0);
+
+Console.WriteLine(similarityScore);
