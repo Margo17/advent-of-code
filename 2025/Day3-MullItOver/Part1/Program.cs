c07bf21dd5e5ce31bfc93dd14fbb523aff79a355
Martynas Goberis <martynasgoberis@gmail.com>
2024-12-03 13:16:47 +0200

    Add Day3 solutions

---
+++ 2025/Day3-MullItOver/Part1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+string corruptedMemory = "";
+int overallMultiplicationResult = 0;
+
+Regex patternToMatchInstructionNumbers = new(@"^mul\((\d+),(\d+)\)$");
+Regex multiplicationInstructionPattern = new(@"mul\(\d+,\d+\)");
+
+while (Console.ReadLine() is string line)
+{
+    corruptedMemory += line;
+}
+
+MatchCollection potentialInstructions = multiplicationInstructionPattern.Matches(corruptedMemory);
+
+foreach (Match instruction in potentialInstructions)
+{
+    string potentialInstruction = instruction.Value;
+
+    if (Regex.IsMatch(potentialInstruction, multiplicationInstructionPattern.ToString()))
+    {
+        Match match = Regex.Match(potentialInstruction, patternToMatchInstructionNumbers.ToString());
+        int number1 = int.Parse(match.Groups[1].Value);
+        int number2 = int.Parse(match.Groups[2].Value);
+
+        overallMultiplicationResult += number1 * number2;
+    }
+}
+
+Console.WriteLine(overallMultiplicationResult);
---
+++ 2025/Day3-MullItOver/Part2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+string corruptedMemory = "";
+int overallMultiplicationResult = 0;
+bool isMultiplicationEnabled = true;
+
+Regex patternToMatchInstructionNumbers = new(@"^mul\((\d+),(\d+)\)$");
+Regex multiplicationsAndConditionalsPattern = new(@"(mul\(\d+,\d+\)|do\(\)|don't\(\))", RegexOptions.IgnoreCase);
+Regex doConditionalPattern = new(@"do\(\)", RegexOptions.IgnoreCase);
+Regex dontConditionalPattern = new(@"don't\(\)", RegexOptions.IgnoreCase);
+
+while (Console.ReadLine() is string line)
+{
+    corruptedMemory += line;
+}
+
+MatchCollection allInstructions = multiplicationsAndConditionalsPattern.Matches(corruptedMemory);
+
+foreach (Match instruction in allInstructions)
+{
+    string potentialInstruction = instruction.Value;
+
+    if (Regex.IsMatch(potentialInstruction, doConditionalPattern.ToString()))
+    {
+        isMultiplicationEnabled = true;
+    }
+    else if (Regex.IsMatch(potentialInstruction, dontConditionalPattern.ToString()))
+    {
+        isMultiplicationEnabled = false;
+    }
+
+    if (Regex.IsMatch(potentialInstruction, patternToMatchInstructionNumbers.ToString()) &&
+        isMultiplicationEnabled)
+    {
+        Match match = Regex.Match(potentialInstruction, patternToMatchInstructionNumbers.ToString());
+        int number1 = int.Parse(match.Groups[1].Value);
+        int number2 = int.Parse(match.Groups[2].Value);
+
+        overallMultiplicationResult += number1 * number2;
+    }
+}
+
+Console.WriteLine(overallMultiplicationResult);
